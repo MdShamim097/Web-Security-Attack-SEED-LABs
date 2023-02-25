00e5712e7580905aa8bd6d4d4d0e23599555d761
Mohammad Shamim Ahsan <shamim19119@gmail.com>
2023-02-25 10:15:14 +0600

    Add files via upload

---
+++ Buffer obverflow (Set-UID) version/Buffer Overflow Attack/Labsetup/code/stack.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/* Changing this size will change the layout of the stack.
+ * Instructors can change this value each year, so students
+ * won't be able to use the solutions from the past.
+ */
+#ifndef BUF_SIZE
+#define BUF_SIZE 100
+#endif
+
+int bof(char *str)
+{
+    char buffer[BUF_SIZE];
+
+    // The following statement has a buffer overflow problem 
+    strcpy(buffer, str);       
+
+    return 1;
+}
+
+int main(int argc, char **argv)
+{
+    char str[517];
+    FILE *badfile;
+
+    badfile = fopen("badfile", "r"); 
+    if (!badfile) {
+       perror("Opening badfile"); exit(1);
+    }
+
+    int length = fread(str, sizeof(char), 517, badfile);
+    printf("Input size: %d\n", length);
+    bof(str);
+    fprintf(stdout, "==== Returned Properly ====\n");
+    return 1;
+}
+
